70d041f00156cc11905177124d79ba9651489418
pincheira07 <m.pincheira07@ufromail.cl>
2024-11-16 15:50:58 -0300

    reddit dag

---
+++ pipelines/reddit_pipeline.py
@@ -0,0 +1,18 @@
+from utils.constants import CLIENT_ID, SECRET, OUTPUT_PATH
+from etls.reddit_etl import connect_reddit, extract_posts, transform_data, load_data_to_csv
+import pandas as pd
+def reddit_pipeline(file_name: str, subreddit: str, time_filter: str, limit: None) -> None:
+    #connecting to reddit instance
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
+    #extraction
+    posts = extract_posts(instance, subreddit, time_filter, limit)
+    post_df = pd.DataFrame(posts)
+
+    #transformation
+    post_df = transform_data(post_df)
+    #loading to csv
+    file_path = f'{OUTPUT_PATH}/{file_name}.csv'
+    print(file_path)
+    load_data_to_csv(post_df,file_path)
+
+    return file_path
